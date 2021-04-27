fefab310f29b1fa5f1ef3fabd4ec17632e90e567
igormatosa <igor.matosa@gmail.com>
2021-04-27 19:57:13 +0200

    operatori

--- src/core/footer/footer.component.ts
+++ src/core/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { from } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { CONSTANTS } from 'src/shared/constants';
 import { Boolean, Q } from 'src/shared/enums';
@@ -23,6 +24,31 @@ export enum Types {
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
+
+  dataArray = [
+    {
+      id: 1,
+      name: 'Pero',
+      surname: 'Preic'
+    },
+    {
+      id: 2,
+      name: 'Marko',
+      surname: 'Preic'
+    },
+    {
+      id: 3,
+      name: 'Marko',
+      surname: 'Markovic'
+    },
+    {
+      id: 4,
+      name: 'Marko',
+      surname: 'Trupkovic'
+    }
+  ];
+
+  numberArray = [2,3,4,5,6];
 
   shared!: string;
   liveShared!: string;
@@ -86,6 +112,57 @@ export class FooterComponent implements OnInit {
       }
     }
 
+    console.log(this.dataArray);
+
+    const person = this.dataArray.find(p => p.id === 1);
+    console.log('1 case: '  + JSON.stringify(person));
+
+    const person2 = this.dataArray.find(p => p.name === 'Pero');
+    console.log('2 case:' + JSON.stringify(person2));
+
+    const person3 = this.dataArray.find(p => p.name === 'Marko');
+    console.log('3 case:' + JSON.stringify(person3));
+
+    const person4 = this.dataArray.find(p => p.name === 'Marko' && p.surname === 'Markovic');
+    console.log('4 case:' + JSON.stringify(person4));
+
+    const persons = this.dataArray.filter(p => p.name === 'Marko');
+    console.log('5 case:' + JSON.stringify(persons));
+
+    const persons2 = this.dataArray.map(p => {
+      return p.name;
+    });
+    console.log('6 case:' + JSON.stringify(persons2));
+
+    const persons3 = this.dataArray.map(p => {
+      return p.name + ' ' + p.surname;
+    });
+    console.log('7 case:' + JSON.stringify(persons3));
+
+    const persons4 = this.dataArray.map(p => {
+      return {
+        value: p.id,
+        text: p.name + ' ' + p.surname 
+      }
+    });
+    console.log('8 case:' + JSON.stringify(persons4));
+
+    const numbers = this.numberArray.map(n => {
+      return n * 10;
+    });
+
+    console.log(this.numberArray);
+    console.log(numbers);
+
+    const obsNumbers = from(this.numberArray);
+
+    console.log(obsNumbers);
+
+    obsNumbers.subscribe(data => {
+      console.log(data);
+    });
+
+    this.numberArray.push(7);
   }
 
 }
